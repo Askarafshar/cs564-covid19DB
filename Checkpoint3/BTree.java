64abdafbfb22730257665aa63e1528d655573296
Askar Safipour Afshar <43677520+Askarafshar@users.noreply.github.com>
2020-07-13 22:23:57 -0500

    Update BTree.java

--- Checkpoint3/BTree.java
+++ Checkpoint3/BTree.java
@@ -31,59 +31,30 @@ class BTree {
      * Search function in the B+Tree.
      * Return recordID for the given StudentID.
      * Otherwise, print out a message that the given studentId has not been found in the table and return -1.
+     *
+     * @param studentId
+     * @return recordID
      */
-    long search(long studentId) {
+long Search(long studentId) {
+        //return -1 if root is empty
+        if (this.root == null) {
+            return -1;
+        }
+
+        //Search for the leaf node which contains the key
+        BTreeNode leaf = searchLeafNode(this.root, studentId);
         long result = -1;
-    	BTreeNode temp;
-    	temp = getNode(this.root, studentId);
-    	long[] keys = temp.getKeys();
-    	for(int i=0;i<keys.length;i++) {
-    		if(studentId == keys[i]) {
-    			result = temp.values[i];
-    		}
-    		if(studentId < keys[i]) {
-    			System.out.println("the given studentId has not been found");
-    			break;
-    		}
-    	}
-    	return result;
+        //find the index and return
+        // Look for value in the leaf
+        for (int i = 0; i < leaf.keys.length; i++) {
+            if (studentId == leaf.keys[i]) {
+                result = leaf.values[i];
+            }
+        }
+        return result;
     }
     
-    //to traverse and get node
-    public BTreeNode getNode(BTreeNode node, long key) {
-    	while(!(node.children==null)) {
-    		node = node.getChild()[childrenSearch(key, node.getKeys())];
-    	}
-    	return node;
-    }
-    //search to consider the internal nodes too.
-    public int childrenSearch(long key, long[] keys) {
-    	int start = 0;
-    	int length = keys.length -1;
-    	int mid;
-    	int index = -1;
-    	if(key < keys[start]) {
-    		return 0;
-    	}
-    	if(key >= keys[length]) {
-    		return keys.length;
-    	}
-    	while(start <= length) {
-    		mid = (start + length) / 2;
-    		if(key < keys[mid] && key >= keys[mid -1]) {
-    			index = mid;
-    			break;
-    		}
-    		else if(key >= keys[mid]) {
-    			start = mid + 1;
-    		}else {
-    			length = mid - 1;
-    		}
-    	}
-    	return index;
-    }
-    
-    /**
+       /**
      * returns the leaf node which might contain key
      *
      * @param key
@@ -117,6 +88,41 @@ class BTree {
         }
     }
     
+   //to traverse and get node
+   // public BTreeNode getNode(BTreeNode node, long key) {
+   // 	while(!(node.children==null)) {
+   // 		node = node.getChild()[childrenSearch(key, node.getKeys())];
+   // 	}
+   //	return node;
+   // }
+   //search to consider the internal nodes too.
+   // public int childrenSearch(long key, long[] keys) {
+   // 	int start = 0;
+   // 	int length = keys.length -1;
+   // 	int mid;
+   // 	int index = -1;
+   // 	if(key < keys[start]) {
+   // 		return 0;
+   // 	}
+   // 	if(key >= keys[length]) {
+   // 		return keys.length;
+   // 	}
+   // 	while(start <= length) {
+   // 		mid = (start + length) / 2;
+   // 		if(key < keys[mid] && key >= keys[mid -1]) {
+   // 			index = mid;
+   // 			break;
+   // 		}
+   // 		else if(key >= keys[mid]) {
+   // 			start = mid + 1;
+   // 		}else {
+   // 			length = mid - 1;
+   // 		}
+   // 	}
+   // 	return index;
+   //}
+    
+     
     BTree insert(Student student) {
         /**
          * TODO:
