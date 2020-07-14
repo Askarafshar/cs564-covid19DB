91bfcaaa19f17fa0dc6e8b64ca343e825727aea6
kevinwalker26 <43677093+kevinwalker26@users.noreply.github.com>
2020-07-14 17:59:27 -0500

    add delete from csv functionality

--- Checkpoint3/BTree.java
+++ Checkpoint3/BTree.java
@@ -1,15 +1,21 @@
+
 /**
  * Do NOT modify.
  * This is the class with the main function
  */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
- * B+Tree Structure
- * Key - StudentId
- * Leaf Node should contain [ key,recordId ]
+ * B+Tree Structure Key - StudentId Leaf Node should contain [ key,recordId ]
  */
 class BTree {
 
@@ -26,27 +32,28 @@ class BTree {
         this.root = null;
         this.t = t;
     }
-    //TODO: review the code again. -ASA
-     /**
-     * Search function in the B+Tree.
-     * Return recordID for the given StudentID.
-     * Otherwise, print out a message that the given studentId has not been found in the table and return -1.
+
+    // TODO: review the code again. -ASA
+    /**
+     * Search function in the B+Tree. Return recordID for the given StudentID.
+     * Otherwise, print out a message that the given studentId has not been found in
+     * the table and return -1.
      *
      * @param studentId
      * @return recordID
      *
      * @author safipourafsh
      */
-long search(long studentId) {
-        //return -1 if root is empty
+    long search(long studentId) {
+        // return -1 if root is empty
         if (this.root == null) {
             return -1;
         }
 
-        //Search for the leaf node which contains the key
+        // Search for the leaf node which contains the key
         BTreeNode leaf = searchLeafNode(this.root, studentId);
         long result = -1;
-        //find the index and return
+        // find the index and return
         // Look for value in the leaf
         for (int i = 0; i < leaf.keys.length; i++) {
             if (studentId == leaf.keys[i]) {
@@ -55,8 +62,8 @@ long search(long studentId) {
         }
         return result;
     }
-    
-       /**
+
+    /**
      * returns the leaf node which might contain key
      *
      * @param key
@@ -65,22 +72,24 @@ long search(long studentId) {
      * @author safipourafsh
      */
     private BTreeNode searchLeafNode(BTreeNode node, long studentId) {
-        //If the node is a leaf, return
+        // If the node is a leaf, return
         if (node.leaf) {
             return node;
         }
-        //If the key is smaller than the first key in the node, follow its first child to continue
+        // If the key is smaller than the first key in the node, follow its first child
+        // to continue
         if (studentId < node.keys[0]) {
             return searchLeafNode(node.children[0], studentId);
-            //If the key is bigger than the last key in the node, follow its last child to continue
-        }else if (studentId > node.keys[node.keys.length - 1]) {
+            // If the key is bigger than the last key in the node, follow its last child to
+            // continue
+        } else if (studentId > node.keys[node.keys.length - 1]) {
             return searchLeafNode(node.children[node.keys.length - 1], studentId);
         } else {
-            //Use binary search to find the child to follow
+            // Use binary search to find the child to follow
             int left = 0, right = node.keys.length - 1, mid;
             while (left <= right) {
                 mid = (left + right) / 2;
-                if (node.keys[mid]==studentId) {
+                if (node.keys[mid] == studentId) {
                     return searchLeafNode(node.children[mid + 1], studentId);
                 } else if (node.keys[mid] < studentId) {
                     left = mid + 1;
@@ -91,115 +100,116 @@ long search(long studentId) {
             return searchLeafNode(node.children[left], studentId);
         }
     }
-    
-   //to traverse and get node
+
+    // to traverse and get node
     public BTreeNode getNode(BTreeNode node, long key) {
-    	while(!(node.children==null)) {
-    		node = node.getChild()[childrenSearch(key, node.getKeys())];
-    	}
-    	return node;
+        while (!(node.children == null)) {
+            node = node.getChild()[childrenSearch(key, node.getKeys())];
+        }
+        return node;
     }
-   //search to consider the internal nodes too.
+
+    // search to consider the internal nodes too.
     public int childrenSearch(long key, long[] keys) {
-    	int start = 0;
-    	int length = keys.length -1;
-    	int mid;
-    	int index = -1;
-    	if(key < keys[start]) {
-    		return 0;
-    	}
-    	if(key >= keys[length]) {
-    		return keys.length;
-    	}
-    	while(start <= length) {
-    		mid = (start + length) / 2;
-    		if(key < keys[mid] && key >= keys[mid -1]) {
-    			index = mid;
-    			break;
-    		}
-    		else if(key >= keys[mid]) {
-    			start = mid + 1;
-    		}else {
-    			length = mid - 1;
-    		}
-    	}
-    	return index;
-   }
-    
-     
-   BTree insert(Student student) {
-        /**
-         * TODO:
-         * Implement this function to insert in the B+Tree.
-         * Also, insert in student.csv after inserting in B+Tree.
-         */    	
-    	if(root == null){
-    		root.keys[0] = student.studentId; 
-    		root.values[0] = student.recordId;
-    	}
-    	return insertHelper(root, student);
+        int start = 0;
+        int length = keys.length - 1;
+        int mid;
+        int index = -1;
+        if (key < keys[start]) {
+            return 0;
+        }
+        if (key >= keys[length]) {
+            return keys.length;
+        }
+        while (start <= length) {
+            mid = (start + length) / 2;
+            if (key < keys[mid] && key >= keys[mid - 1]) {
+                index = mid;
+                break;
+            } else if (key >= keys[mid]) {
+                start = mid + 1;
+            } else {
+                length = mid - 1;
+            }
+        }
+        return index;
     }
-    
-    BTree insertHelper(BTreeNode node, Student student) { 
-    	long studentId = student.studentId;
-    	long recordId = student.recordId;
-    	BTreeNode candidate = searchLeafNode(node, studentId);
-    	int index = childrenSearch(studentId, candidate.keys);
-    	//if there is room for the key
-    	if(!capacity(candidate)) {	    	
-	    	for(int i=candidate.values.length-2; i>=index; i--) {
-	    		candidate.keys[i+1] = candidate.keys[i];
-	    		candidate.values[i+1] = candidate.values[i];
-	    	}
-	    	candidate.keys[index] = studentId;
-	    	candidate.values[index] = recordId;
-    	}
-    	//if the node if full we need split the node
-    	else {
-    		if(candidate == this.root) {
-    			BTreeNode newRoot = new BTreeNode(t, true);
-    			newRoot.children[0] = candidate;
-    		}
-    		BTreeNode newNode = new BTreeNode(t, true);
-    		newNode.keys[0] = studentId; 
-    		for(int i=index; i<candidate.keys.length; i++) {
-    			newNode.keys[i-index+1] = candidate.keys[i];
-    			candidate.keys[i] = 0;    			
-    		}
-    		newNode.next = candidate.next;
-    		candidate.next = newNode;
-    		
-    		BTreeNode parent = findParent(studentId);
-    		insertHelper(parent, student);    		
-    	}    
-    	
-    	FileWriter writer = new FileWriter("../student.csv");
-    	//Writing data to the csv file
-    	writer.append(String.join(",", student));//it needs to get fixed
+
+    BTree insert(Student student) {
+        /**
+         * TODO: Implement this function to insert in the B+Tree. Also, insert in
+         * student.csv after inserting in B+Tree.
+         */
+        if (root == null) {
+            root.keys[0] = student.studentId;
+            root.values[0] = student.recordId;
+        }
+        return insertHelper(root, student);
+    }
+
+    BTree insertHelper(BTreeNode node, Student student) {
+        long studentId = student.studentId;
+        long recordId = student.recordId;
+        BTreeNode candidate = searchLeafNode(node, studentId);
+        int index = childrenSearch(studentId, candidate.keys);
+        // if there is room for the key
+        if (!capacity(candidate)) {
+            for (int i = candidate.values.length - 2; i >= index; i--) {
+                candidate.keys[i + 1] = candidate.keys[i];
+                candidate.values[i + 1] = candidate.values[i];
+            }
+            candidate.keys[index] = studentId;
+            candidate.values[index] = recordId;
+        }
+        // if the node if full we need split the node
+        else {
+            if (candidate == this.root) {
+                BTreeNode newRoot = new BTreeNode(t, true);
+                newRoot.children[0] = candidate;
+            }
+            BTreeNode newNode = new BTreeNode(t, true);
+            newNode.keys[0] = studentId;
+            for (int i = index; i < candidate.keys.length; i++) {
+                newNode.keys[i - index + 1] = candidate.keys[i];
+                candidate.keys[i] = 0;
+            }
+            newNode.next = candidate.next;
+            candidate.next = newNode;
+
+            BTreeNode parent = findParent(studentId);
+            insertHelper(parent, student);
+        }
+
+        FileWriter writer = new FileWriter("../student.csv");
+        // Writing data to the csv file
+        writer.append(String.join(",", student));// it needs to get fixed
         writer.append("\n");
-        //Flushing data from writer to file
+        // Flushing data from writer to file
         writer.flush();
         writer.close();
         System.out.println("Data entered");
-        
+
         return this;
-    }
-    /**
-     * returns true if the node is full and false otherwise 
-     */
-    public boolean capacity(BTreeNode node) {
-    	if(node.keys.length == node.values.length) {return true;}
-    	return false;
     }
 
     /**
-     * Deletes an entry from the B+ tree given a studentId. Then it attempts to remove
-     * the entry from student.csv. 
+     * returns true if the node is full and false otherwise
+     */
+    public boolean capacity(BTreeNode node) {
+        if (node.keys.length == node.values.length) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Deletes an entry from the B+ tree given a studentId. Then it attempts to
+     * remove the entry from student.csv.
      * 
-     * @param studentId     id of the student to be deleted
-     * @return true     if the student was successfully deleted from the B+ tree
-     *                      does not guarantee it was removed from student.csv
-     *         false    if student does not exist or failed delete
+     * @param studentId id of the student to be deleted
+     * @return true if the student was successfully deleted from the B+ tree does
+     *         not guarantee it was removed from student.csv false if student does
+     *         not exist or failed delete
      * @author kwalker26
      */
     boolean delete(long studentId) {
@@ -207,7 +217,7 @@ long search(long studentId) {
         int studIndex = 0;
 
         if (root == null) {
-            return false;   // empty tree
+            return false; // empty tree
         }
 
         // find the node with the student
@@ -218,10 +228,9 @@ long search(long studentId) {
                 // we found the student entry to delete
                 studIndex = i;
                 break;
-    		}
-    		else if (studentId < studKeys[i]) {
+            } else if (studentId < studKeys[i]) {
                 // student entry not found
-    			System.out.println("The given studentId was not found");
+                System.out.println("The given studentId was not found");
                 return false;
             }
         }
@@ -266,7 +275,35 @@ long search(long studentId) {
         }
 
         // attempt to delete from .csv
-
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("./student.csv"));
+            List<String> validEntries = new ArrayList<String>();
+            String currLine, currID;
+            while ((currLine = buffer.readLine()) != null) {
+                if (currLine.length() != 0) {
+                    currID = currLine.split(",")[0];
+                    if (studentId != Long.parseLong(currID)) {
+                        // only add if not the student that was deleted
+                        validEntries.add(currLine);
+                    }
+                }
+            }
+            buffer.close(); // close once we are done reading
+            // rewrite all entries that were added to the list
+            FileWriter writer = new FileWriter("./student.csv", false);
+            Iterator<String> iter = validEntries.iterator();
+            while (iter.hasNext()) {
+                writer.write(iter.next() + "\n");
+            }
+            writer.flush();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Failed to find student.csv for update upon delete.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error while updating student.csv upon delete.");
+            e.printStackTrace();
+        }
 
         return true;
     }
