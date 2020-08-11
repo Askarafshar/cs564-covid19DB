3121c8758c7446e63f22edbf2b8439507a74f48d
Askar Safipour Afshar <43677520+Askarafshar@users.noreply.github.com>
2020-08-10 20:37:13 -0500

    Update DBConnection.java

--- databases_app/src/swing/DBConnection.java
+++ databases_app/src/swing/DBConnection.java
@@ -1,3 +1,20 @@
+///////////////////////////////// FILE HEADER /////////////////////////////////
+//
+// Title:           COVID19 Database
+// This File:       DBConnection.java
+// Files:           Editor.java, EqualityOperator.java, DBConnection.java, CommonMethos.java
+//
+// Private GitHub Repo:    https://github.com/Askarafshar/cs564-covid19DB
+//
+// Authors                    Email:
+// Askar Safipour Afshar      safipourafsh@wisc.edu
+// Elaheh Jabbarifard         jabbarifard@wisc.edu
+// Kevin Walker               kwalker26@wisc.edu
+//
+// Lecturer's Name:    HIEN NGUYEN
+// Course:             CS564, Summer 2020
+//
+/////////////////////////////////////////////////////////////////////////////
 package swing;
 
 import java.util.*;
